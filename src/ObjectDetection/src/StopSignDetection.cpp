1b206a63a5b35dc002f9911fee70098d8af270b6
hanienkobus <hanienkobus@gmail.com>
2019-05-13 15:51:36 +0200

    old stop sign detection reversion

--- src/ObjectDetection/src/StopSignDetection.cpp
+++ src/ObjectDetection/src/StopSignDetection.cpp
@@ -19,22 +19,19 @@ using namespace std;
 
 class StopSignDetection {
 
-  bool VERBOSE = false;
-
   double minStopSignArea = 1000;
   int number_of_missed_signs = 0;
-  int max_number_of_missed_signs = 4;
+  int max_number_of_missed_signs = 30;
 
   bool STOPSIGN_FOUND = false;
   double area = 0;
-  vector<Point> current_contour{};
 
-  double getBiggestOctagon(Mat image);
-  string detect(vector<Point> input);
+  vector<Point> getBiggestOctagon(const Mat &image);
+  string detect(const vector<Point> &input);
   void followStopsign();
   void lookForStopSign();
-  double getArea(vector<Point> input);
-  Point getCenter(vector<Point> input);
+  double getArea(const vector<Point> &input);
+  Point getCenter(const vector<Point> &input);
   bool same(double a, double b);
 
 public:
@@ -42,23 +39,32 @@ public:
   int failed_frames = 0;
   void setNumberOfMissedSign(int numberOfMissedSigns);
   void setMinArea(double input);
-  void setArea(double input);
+  void setArea(double &input);
 
-  void showAllShapes(Mat image);
-  void run(Mat image, bool verbose, bool VIDEO);
+  void showAllShapes(const Mat &image);
+  void run(const Mat &image, bool VERBOSE, bool VIDEO);
 };
 
-void StopSignDetection::run(Mat image, bool verbose, bool VIDEO) {
-  VERBOSE = verbose;
-  Mat img;
-  Rect myROI(0,0,image.size().width , image.size().height * 3/4);
-  Mat croppedIamge = image(myROI);
-  
-  Rect myROI1(croppedIamge.size().width,0,croppedIamge.size().width /2 , image.size().height );
-  Mat croppedIamge1 = image(myROI1);
-  cvtColor(croppedIamge1 ,img , COLOR_BGR2GRAY );
+void StopSignDetection::run(const Mat &image, bool VERBOSE, bool VIDEO) {
 
-  area = getBiggestOctagon(img);
+  // Setup a rectangle to define your region of interest
+  Rect myROI1(0, 0, image.size().width, image.size().height * 3 / 4);
+  // Crop the full image to that image contained by the rectangle myROI
+  Mat croppedImage1 = image(myROI1);
+
+  Rect myROI2(croppedImage1.size().width / 2, 0, croppedImage1.size().width / 2,
+              croppedImage1.size().height);
+  Mat croppedImage2 = image(myROI2);
+
+  Mat img;
+  cvtColor(croppedImage2, img, COLOR_BGR2GRAY);
+
+  vector<Point> contour = getBiggestOctagon(img);
+  area = getArea(contour);
+
+  if (VERBOSE) {
+    //cout << "Current Octagon area :" << area << endl;
+  }
 
   if (STOPSIGN_FOUND) {
     followStopsign();
@@ -66,22 +72,23 @@ void StopSignDetection::run(Mat image, bool verbose, bool VIDEO) {
     lookForStopSign();
   }
 
+  // Display image
   if (VIDEO) {
     Mat drawing = img.clone();
+    if (area >= minStopSignArea) {
+      Point pt = getCenter(contour);
 
-    Point pt = getCenter(current_contour);
-
-    putText(drawing, "octagon", pt, FONT_HERSHEY_SIMPLEX, 0.5,
-            Scalar(255, 255, 255), 2);
-    Scalar color = Scalar(0, 0, 255);
-    polylines(drawing, current_contour, true, color, 1, 8);
-
+      putText(drawing, "octagon", pt, FONT_HERSHEY_SIMPLEX, 0.5,
+              Scalar(255, 255, 255), 2);
+      Scalar color = Scalar(0, 0, 255);
+      polylines(drawing, contour, true, color, 1, 8);
+    }
     imshow("StopSignDetection Vision", drawing);
     waitKey(1);
   }
 }
 
-void StopSignDetection::setArea(double input) { area = input; }
+void StopSignDetection::setArea(double &input) { area = input; }
 
 void StopSignDetection::setMinArea(double input) { minStopSignArea = input; }
 
@@ -89,14 +96,14 @@ void StopSignDetection::setNumberOfMissedSign(int numberOfMissedSigns) {
   max_number_of_missed_signs = numberOfMissedSigns;
 }
 
-Point StopSignDetection::getCenter(vector<Point> input) {
+Point StopSignDetection::getCenter(const vector<Point> &input) {
   Moments M = moments(input);
   int X = static_cast<int>(M.m10 / M.m00);
   int Y = static_cast<int>(M.m01 / M.m00);
   return Point(X, Y);
 }
 
-double StopSignDetection::getArea(vector<Point> input) {
+double StopSignDetection::getArea(const vector<Point> &input) {
   try {
     return contourArea(input);
   } catch (...) {
@@ -122,25 +129,25 @@ bool StopSignDetection::same(double a, double b) {
 void StopSignDetection::followStopsign() {
   //: Counts the number of times we haven't seen the stopsign
   if (same(area, 0.0)) {
-    // cout << "Failed frame." << endl;
+    //cout << "Failed frame." << endl;
   } else if (area >= minStopSignArea) {
     number_of_missed_signs = 0;
-    // cout << "-- Stop sign found , reseting counter ---- " << endl;
+    //cout << "-- Stop sign found , reseting counter ---- " << endl;
   } else if (area < minStopSignArea) {
     number_of_missed_signs++;
-    //  cout << "Not seeing stop sign . Frames not seen : "
-    //       << number_of_missed_signs << endl;
+    //cout << "Not seeing stop sign . Frames not seen : "
+     //    << number_of_missed_signs << endl;
   }
 
   //: Returns true ( signaling that we are still following a stopsign) if we
-  // haven't seen it in a while
+  //haven't seen it in a while
   if (number_of_missed_signs >= max_number_of_missed_signs) {
     StopSignDetection::Threshhold_reached = true;
-    //  cout << "-- Threshhold reached , stoping car ---- " << endl;
+  //  cout << "-- Threshhold reached , stoping car ---- " << endl;
   }
 }
 
-string StopSignDetection::detect(vector<Point> input) {
+string StopSignDetection::detect(const vector<Point> &input) {
 
   string shape = "unidentified";
   const int num_of_vertices = input.size();
@@ -159,15 +166,16 @@ string StopSignDetection::detect(vector<Point> input) {
     shape = "octagon";
   } else if (num_of_vertices == 9) {
     shape = "nonagon";
-  } else if (num_of_vertices == 10) {
+  } else if (num_of_vertices == 5) {
     shape = "decagon";
   }
   return shape;
 }
 
-double StopSignDetection::getBiggestOctagon(Mat image) {
+vector<Point> StopSignDetection::getBiggestOctagon(const Mat &image) {
 
   Mat blured_image;
+
   blur(image, blured_image, Size(3, 3));
   Canny(blured_image, blured_image, 80, 240, 3);
 
@@ -175,7 +183,7 @@ double StopSignDetection::getBiggestOctagon(Mat image) {
   findContours(blured_image, contours, CV_RETR_EXTERNAL,
                CV_CHAIN_APPROX_SIMPLE);
 
-  double biggest = 0.0;
+  double biggest;
   vector<Point> bigC;
 
   string shape;
@@ -193,11 +201,10 @@ double StopSignDetection::getBiggestOctagon(Mat image) {
       bigC = approx;
     }
   }
-  current_contour = bigC;
-  return biggest;
+  return bigC;
 }
 
-void StopSignDetection::showAllShapes(Mat image) {
+void StopSignDetection::showAllShapes(const Mat &image) {
 
   Mat blured_image;
 
